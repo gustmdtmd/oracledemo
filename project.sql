33b50a8e068d2371319ac09954e0b80ca29f853a
hs <kaos5930@gmail.com>
2022-11-03 22:07:30 +0900

    20221103 | update

---
+++ project.sql
@@ -0,0 +1,17 @@
+--테이블 생성
+ CREATE TABLE book (
+     num number constraint book_num_pk primary key,
+     title varchar2(100),
+     author varchar2(50)
+ );
+
+--시퀀스 생성
+ CREATE SEQUENCE book_num_seq
+ START WITH 1
+ INCREMENT BY 1
+ NOCACHE
+ NOCYCLE;
+ 
+--삽입
+ INSERT INTO book(num, title, author)
+ VALUES(book_num_seq.nextval, '홍길동전', '허균');
